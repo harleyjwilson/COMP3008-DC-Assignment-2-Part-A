d91fbcb8f578a545d0ece609786fadbd7711d002
Harley Wilson <54230223+harleyjwilson@users.noreply.github.com>
2023-09-23 21:14:07 +0800

    Add WithdrawController

---
+++ LocalDBWebApiUsingEF/Controllers/WithdrawController.cs
@@ -0,0 +1,68 @@
+using LocalDBWebApiUsingEF.Data;
+using LocalDBWebApiUsingEF.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace LocalDBWebApiUsingEF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class WithdrawController : ControllerBase
+    {
+        private readonly DBManager _context;
+
+        public WithdrawController(DBManager context)
+        {
+            _context = context;
+        }
+        // POST: api/deposit
+        [HttpPost]
+        public async Task<ActionResult<BankAccount>> ProcessWithdrawal(Transaction transact)
+        {
+            var account = await _context.BankAccounts
+                                     .FirstOrDefaultAsync(b => b.AccountNumber == transact.AccountNumber);
+            if (account == null)
+            {
+                return NotFound();
+            }
+
+            if (transact.Amount == 0.0)
+            {
+                return BadRequest("Invalid amount - Can't withdraw 0.0");
+            }
+            else if (transact.Amount > 0.0)
+            {
+                transact.Amount = -transact.Amount;
+            }
+
+            double newBalance = account.Balance + transact.Amount;
+
+            if (newBalance >= 0.0)
+            {
+                account.Balance = newBalance;
+                account.Transactions.Add(transact);
+                _context.Entry(account).State = EntityState.Modified;
+                try
+                {
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!_context.BankAccounts.Any(e => e.AccountNumber == transact.AccountNumber))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return NoContent();
+            }
+            else
+            {
+                return BadRequest("Insufficient funds");
+            }
+        }
+    }
+}
