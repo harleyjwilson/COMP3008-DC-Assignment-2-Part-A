fee0c60fe204d0b1172ff315d0918149813afff2
Victor Marteli <caio.marteli@proton.me>
2023-10-09 14:09:00 +0800

    reintroduced post methods to Deposit  and Withdraw Controllers.

--- WebApi/Controllers/DepositController.cs
+++ WebApi/Controllers/DepositController.cs
@@ -1,5 +1,7 @@
 using LocalDBWebApiUsingEF.Data;
+using LocalDBWebApiUsingEF.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LocalDBWebApiUsingEF.Controllers {
     [Route("api/[controller]")]
@@ -9,6 +11,33 @@ namespace LocalDBWebApiUsingEF.Controllers {
 
         public DepositController(DBManager context) {
             _context = context;
+        }
+
+        // POST: api/deposit
+        [HttpPost]
+        public async Task<ActionResult<BankAccount>> ProcessDeposit(Transaction transact) {
+            var account = await _context.BankAccounts
+                                     .FirstOrDefaultAsync(b => b.AccountNumber == transact.FromAccountNumber);
+            if (account == null) {
+                return NotFound("Bank account not found.");
+            }
+            if (transact.Amount == 0.0) {
+                return BadRequest("Invalid amount - Can't deposit 0.0");
+            }
+            account.Balance = account.Balance + transact.Amount;
+            account.Transactions.Add(transact);
+            _context.Entry(account).State = EntityState.Modified;
+            try {
+                await _context.SaveChangesAsync();
+            } catch (DbUpdateConcurrencyException) {
+                if (!_context.BankAccounts.Any(e => e.AccountNumber == transact.FromAccountNumber)) {
+                    return NotFound("Bank account not found. ");
+                }
+                else {
+                    throw;
+                }
+            }
+            return NoContent();
         }
 
     }
--- WebApi/Controllers/WithdrawController.cs
+++ WebApi/Controllers/WithdrawController.cs
@@ -1,5 +1,7 @@
 using LocalDBWebApiUsingEF.Data;
+using LocalDBWebApiUsingEF.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace LocalDBWebApiUsingEF.Controllers {
     [Route("api/[controller]")]
@@ -11,6 +13,43 @@ namespace LocalDBWebApiUsingEF.Controllers {
             _context = context;
         }
         // POST: api/deposit
+        [HttpPost]
+        public async Task<ActionResult<BankAccount>> ProcessWithdrawal(Transaction transact) {
+            var account = await _context.BankAccounts
+                                     .FirstOrDefaultAsync(b => b.AccountNumber == transact.FromAccountNumber);
+            if (account == null) {
+                return NotFound("Bank account not found.");
+            }
+
+            if (transact.Amount == 0.0) {
+                return BadRequest("Invalid amount - Can't withdraw 0.0");
+            }
+            else if (transact.Amount > 0.0) {
+                transact.Amount = -transact.Amount;
+            }
+
+            double newBalance = account.Balance + transact.Amount;
+
+            if (newBalance >= 0.0) {
+                account.Balance = newBalance;
+                account.Transactions.Add(transact);
+                _context.Entry(account).State = EntityState.Modified;
+                try {
+                    await _context.SaveChangesAsync();
+                } catch (DbUpdateConcurrencyException) {
+                    if (!_context.BankAccounts.Any(e => e.AccountNumber == transact.FromAccountNumber)) {
+                        return NotFound("Bank account not found.");
+                    }
+                    else {
+                        throw;
+                    }
+                }
+                return NoContent();
+            }
+            else {
+                return BadRequest("Insufficient funds.");
+            }
+        }
 
     }
 }
